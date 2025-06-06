796142cda40fd3b4aa26382a72e7f56b1fac9de7
Abhishek Purushothama <ap2089@georgetown.edu>
2025-06-06 17:18:35 -0400

    Add dataset loading code

---
+++ run_model.py
@@ -0,0 +1,95 @@
+import argparse
+import io
+from torch import nn
+from torch.utils.data import DataLoader
+from transformers import AutoModel, Trainer, AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, \
+    TrainingArguments
+from pathlib import Path
+from datasets import Dataset, load_dataset, DatasetDict
+
+# Ref : https://github.com/disrpt/sharedtask2025/blob/091404690ed4912ca55873616ddcaa7f26849308/utils/disrpt_eval_2024.py#L246
+def load_rels_dataset(dev_filepath, train_filepath):
+    # not compatible with a the pandas based \t reader try with explicit code
+    # return load_dataset('csv', data_files={'dev': dev_filepath, 'train': train_filepath}, delimiter='\t')
+    def get_dataset(filepath):
+        data = io.open(filepath, encoding="utf-8").read().strip().replace("\r", "")
+        lines = data.split("\n")
+        header = lines[0]
+        split_lines = [line.split("\t") for line in lines[1:]]
+        LABEL_ID = -1
+        TYPE_ID = -3
+        U1_ID = 5
+        U2_ID = 6
+        DIRECTION_ID = -4
+
+        labels = [line[LABEL_ID] for line in split_lines]
+        u1s = [line[U1_ID] for line in split_lines]
+        u2s = [line[U2_ID] for line in split_lines]
+        directions = [line[DIRECTION_ID] for line in split_lines]
+        types = [line[TYPE_ID] for line in split_lines]
+
+        return Dataset.from_dict({
+            "label": labels,
+            "type": types,
+            "u1": u1s,
+            "u2": u2s,
+            "direction": directions,
+        })
+
+
+    dev_rels = get_dataset(dev_filepath)
+    train_rels = get_dataset(train_filepath)
+    print(dev_rels[0])
+    print(train_rels[0])
+    dataset = DatasetDict()
+    dataset['dev'] = dev_rels
+    dataset['train'] = train_rels
+    return dataset
+
+# Fix doesn't work yet
+# TODO pab make it a pipeline
+# Ref: https://gitlab.irit.fr/melodi/andiamo/discret-zero-shot/-/blob/master/classifier_pytorch.py
+class TransformerClassifier(nn.Module):
+    def __init__(self, num_labels = 17, model_name='google-bert/bert-base-multilingual-cased'):
+        super(TransformerClassifier, self).__init__()
+        self.model = AutoModel.from_pretrained(model_name)
+        self.linear = nn.Linear(768, num_labels)
+
+    def forward(self, input_id, mask):
+        """Only trains the final linear layer"""
+        outputs = self.model(input_ids=input_id, attention_mask=mask)
+        outputs = self.linear(outputs.last_hidden_state)
+        return outputs
+
+def train(model_name, dev_dataset, train_dataset):
+    """The function uses huggingface to train model with dataset"""
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=17)
+    training_args = TrainingArguments(
+        output_dir="./results",
+        learning_rate=2e-5,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        num_train_epochs=5,
+    )
+    trainer = Trainer(
+        model=model,
+        training_args=training_args,
+        train_dataset=train_dataset,
+        validation_dataset=dev_dataset,
+        collate_fn=collator,
+    )
+    trainer.train()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dev", type=str)
+    parser.add_argument("train", type=str)
+    parser.add_argument("model_name", type=str)
+    args = parser.parse_args()
+    dataset = load_rels_dataset(args.dev, args.train)
+    # train(model_name=args.model_name, dev_dataset=dataset, train_dataset=dataset)
+    print(dataset)
+
+
